bfcf6e9c1ab8b2dba03b8f3d611e7d17d58a4406
valmg11 <tictactina05@gmail.com>
2026-04-24 10:44:54 -0400

    navbar creation

---
+++ portfolio/src/components/Navbar.js
@@ -0,0 +1,18 @@
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h3>
+                    Hi
+                </h3>
+                <ul>
+                    <li>
+                        <a href="#about">Projects</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
